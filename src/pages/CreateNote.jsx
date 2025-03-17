691b9275395beecb517e6a04cc7675e97975f8a2
karan <karan372003@gmail.com>
2025-03-18 04:21:34 +0530

    made all the pages

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { Link } from "react-router-dom"
+import { FaArrowLeft } from "react-icons/fa";
 
 function CreateNote() {
   return (
-    <div>CreateNote</div>
+    <>
+      <section className="text-white p-3 bg-black h-[100vh]">
+        <header className="flex justify-around h-[5%] ">
+          <Link className="p-3 text-2xl flex justify-center items-center bg-gray-500 text-center rounded-md" to={"/"}><FaArrowLeft /></Link>
+          <button className="btn text-2xl p-3 rounded-md flex justify-center items-center bg-gray-500">Save</button>
+        </header>
+        <form className="create flex flex-col h-[95%] bg-blue-800 mt-2">
+            <input className="p-3 text-2xl outline-none h-[5%] bg-purple-600" type="text" placeholder="Title..." autoFocus />
+            <textarea className="p-3 text-2xl outline-none h-[95%] bg-gray-400" placeholder="Details..."></textarea>
+        </form>
+      </section>
+    </>
   )
 }
 
