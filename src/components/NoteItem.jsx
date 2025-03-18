0a2508d2340d8102fcfc26fb23bfccb491f3589d
karan <karan372003@gmail.com>
2025-03-19 02:48:46 +0530

    added search funtionality

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -2,10 +2,10 @@ import { Link } from 'react-router-dom'
 
 function NoteItem({note}) {
   return (
-    <Link to={`/edit/${note.id}`} className="note flex flex-col justify-between rounded-md p-1 max-h-[130px] bg-blue-900 w-[190px] text-white lg:w-[398px]">
-        <h4 className='p-2 max-h-[60px]  bg-blue-700 '>{note.title.length > 30 ? (note.title.substr(0,30)) + "..." : note.title}</h4>
-        <p className='bg-blue-600  max-h-[70px] overflow-hidden'>{note.details}</p>
-        <p className='bg-blue-400'>{note.date}</p>
+    <Link to={`/edit/${note.id}`} className="note flex flex-col justify-between rounded-[1rem] p-2 max-h-[130px] bg-gray-900 w-[190px] text-white lg:w-[398px]">
+        <h4 className='p-1 max-h-[30px] rounded-[6px]  bg-gray-700 '>{note.title.length > 30 ? (note.title.substr(0,30)) + "..." : note.title}</h4>
+        <p className='bg-graay-600 h-[60px]  max-h-[70px] overflow-hidden'>{note.details}</p>
+        <p className='text-[11px]'>{note.date}</p>
     </Link>
   )
 }
--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -2,25 +2,43 @@ import { FaSearch } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import { FaPlus } from "react-icons/fa";
 import NoteItem from "../components/NoteItem";
+import { useEffect, useState } from "react";
+import { IoCloseSharp } from "react-icons/io5";
 
 function Notes({notes}) {
+  const [showSearch,setShowSearch] = useState(false)
+
+  const [text,setText] = useState("")
+  const [filterNotes,setFilterNotes] = useState(notes)
+
+  const handleSearch =() =>{
+    setFilterNotes(notes.filter((note)=>{
+      if(note.title.toLowerCase().match(text.toLowerCase())){
+        return note;
+      }
+    }))
+  }
+
+  useEffect(handleSearch,[text])
+
+
   return (
     <>
-      <section className="bg-black h-[100vh] text-white relative">
+      <section className=" h-[100vh]  text-white relative">
         <header className="flex justify-around p-2">
-          <h2 className="text-3xl font-bold ">QuickMemo</h2>
-          {/* <input className="border rounded-md px-1" type="text" autoFocus placeholder='keyword...' /> */}
-          <button className="bg-gray-600 border text-white p-2.5 rounded-md"><FaSearch /></button>
+         {!showSearch ? <h2 className="text-3xl font-bold ">QuickMemo</h2> :
+          <input value={text} onChange={(e) => {setText(e.target.value);}} className="border rounded-md px-1" type="text" autoFocus placeholder='keyword...' />}
+          <button onClick={()=>setShowSearch(!showSearch)} className="bg-gray-600 border text-white p-2.5 rounded-md">{showSearch ? <IoCloseSharp />  : <FaSearch />}</button>
         </header>
 
-        <div className="container flex flex-wrap justify-center p-2 gap-3">
+        <div className="flex flex-wrap justify-center p-2 gap-3 ">
           {
-            notes.map((note) => {
+            filterNotes.map((note) => {
               return <NoteItem key={note.id} note={note}/>
             })
           }
         </div>
-        <Link to={"/create"} className="p-4 text-3xl rounded-md absolute bottom-2 right-2 bg-gray-600"><FaPlus /></Link>
+        <Link to={"/create"} className="p-4 text-3xl rounded-md fixed bottom-2 right-2 bg-gray-600"><FaPlus /></Link>
       </section>
     </>
   )
