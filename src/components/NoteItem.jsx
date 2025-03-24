6445f0f50416c1ad21a7ec0c27acc22825ecdac5
karan <karan372003@gmail.com>
2025-03-24 07:08:50 +0530

    made some ui changes

--- src/components/NoteItem.jsx
+++ src/components/NoteItem.jsx
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom'
 function NoteItem({note}) {
   return (
     <Link to={`/edit/${note.id}`}
-    className="note flex flex-col justify-between rounded-[1rem] p-2 max-h-[130px] gap-[3px] bg-gray-900 w-[180px]   drop-shadow-xl text-white lg:w-[398px] inset-shadow-sm inset-shadow-gray-500/50">
-        <h4  className=' p-1 max-h-[30px] rounded-[10px]  bg-gray-700 shadow-2xl inset-shadow-sm inset-shadow-gray-500/50 shadow-gray-500/50'>{note.title.length > 30 ? (note.title.substr(0,30)) + "..." : note.title}</h4>
-        <p  className='bg-graay-600 h-[60px] p-1  max-h-[70px] overflow-hidden inset-shadow-sm  inset-shadow-gray-500/50 rounded-xl'>{note.details}</p>
+    className="note flex flex-col justify-between  border-b-[#0b0f16] border rounded-[0.5rem] p-2 max-h-[130px] gap-[3px] bg-gradient-to-b from-[#0d3875] to-[#0b0f16] w-[180px] drop-shadow-xl text-white lg:w-[398px] inset-shadow-sm inset-shadow-gray-500/50">
+        <h4  className=' p-1 max-h-[30px] rounded-[4px]  bg-[#213f74] shadow-2xl inset-shadow-sm inset-shadow-gray-500/50 shadow-gray-500/50'>{note.title.length > 30 ? (note.title.substr(0,30)) + "..." : note.title}</h4>
+        <p  className=' h-[60px] p-1  max-h-[70px]  overflow-hidden rounded-xl'>{note.details}</p>
         <p className='text-[11px] '>{note.date}</p>
     </Link>
   )
--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -35,14 +35,14 @@ function CreateNote({setNotes}) {
 
   return (
     <>
-      <section className="text-white p-3 bg-[#1b1d30] h-[100vh]">
+      <section className="text-white p-3 bg-gradient-to-b from-[#0d3875] to-[#0b0f16] h-[100vh]">
         <header className="flex justify-around h-[5%] ">
-          <Link className="p-3 text-2xl flex justify-center items-center bg-gray-700 text-center rounded-md brightness-150 inset-shadow-sm inset-shadow-gray-500/50" to={"/"}><FaArrowLeft /></Link>
-          <button onClick={handleSubmit} className="btn text-2xl p-3 rounded-md flex justify-center items-center bg-gray-700 brightness-150 inset-shadow-sm inset-shadow-gray-500/50">Save</button>
+          <Link className="p-3 text-2xl flex justify-center items-center bg-[#081d3c] text-center rounded-md brightness-150 inset-shadow-sm inset-shadow-gray-500/50" to={"/"}><FaArrowLeft /></Link>
+          <button onClick={handleSubmit} className=" text-2xl p-3  rounded-md flex justify-center items-center bg-[#081d3c] inset-shadow-sm inset-shadow-gray-500/50">Save</button>
         </header>
         <form className="create flex flex-col h-[95%] gap-[2px]  mt-2">
-            <input onChange={(e)=>setTitle(e.target.value)} value={title}  className="inset-shadow-sm inset-shadow-gray-500/50 p-3 text-2xl brightness-150 outline-none h-[5%] bg-gray-700 rounded-xl" type="text" placeholder="Title..." autoFocus />
-            <textarea onChange={(e)=>setDetails(e.target.value)} value={details} className="inset-shadow-sm inset-shadow-gray-500/50 p-3 text-2xl outline-none h-[95%] bg-gray-600 rounded-xl" placeholder="Details..."></textarea>
+            <input onChange={(e)=>setTitle(e.target.value)} value={title}  className="inset-shadow-sm inset-shadow-gray-500/50 p-3 text-2xl brightness-150 outline-none h-[5%] bg-[#213f74] rounded-xl" type="text" placeholder="Title..." autoFocus />
+            <textarea onChange={(e)=>setDetails(e.target.value)} value={details} className="inset-shadow-sm inset-shadow-gray-500/50 p-3 text-2xl outline-none h-[95%]  border border-b-[#0b0f16] rounded-xl" placeholder="Details..."></textarea>
         </form>
       </section>
     </>
